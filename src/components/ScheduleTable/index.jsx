1a6631ba3b453238cb10027b7c1e9177ad7b8890
Darlan Mendes <dedsondarlan@gmail.com>
2023-03-21 12:55:02 -0300

    Adição de função de busca de coleçoes

--- src/components/ScheduleTable/index.jsx
+++ src/components/ScheduleTable/index.jsx
@@ -2,18 +2,41 @@ import styles from './styles.module.scss';
 import Modal from '../Modal'
 import { useEffect, useState } from 'react';
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
+import { collection, query, where, getDocs,getFirestore} from "firebase/firestore";
+import {app} from '../../db/firebaseConf'
 export default function ScheduleTable(props) {
     let manha = ['07h00 - 07h50', '07h50 - 08h40', '08h40 - 09h50', '9h50 - 10h40', '10h40 - 11h30', '11h30 - 12h20']
     let tarde = ['13h00 - 13h50', '13h50 - 14h40', '14h40 - 15h50', '15h50 - 16h40', '16h40 - 17h30', '17h30 - 18h20']
     let noite = ['18h30 - 19h15', '19h15 - 20h00', '20h00 - 20h45', '20h45 - 21h30']
     const [data, setData] = useState(new Date());
     const [schedules, setSchedules] = useState(manha)
-    const [modal, setModal]=useState(false)
+    const [modal, setModal] = useState(false)
+    const[reserves, setReserves]=useState()
+    const db = getFirestore(app);
+    async function receberLista() {
+        
+        const querySnapshot = await getDocs(collection(db, "reserva"), where("data", "==", data));
+       
+        querySnapshot.forEach((doc) => {
+           // console.log(doc.id, " => ", doc.data());
+        });
+        setReserves(querySnapshot)
+    }
     const mudarDia = (dias) => {
         const novaData = new Date(data);
         novaData.setDate(novaData.getDate() + dias);
         setData(novaData);
     };
+    function formatarData(){
+        return `${(data.getDate() < 10 ? '0' + (data.getDate()) : data.getDate() )+'/'+ (data.getMonth() + 1 < 10 ? '0' + (data.getMonth() + 1) : data.getMonth() + 1)+'/'+data.getFullYear()}`
+    }
+    useEffect(() => {
+        
+        receberLista()
+    },[data])
+
+
+   
     const handleSchedule = (e) => {
         if (e.target.value === 'manha') {
             setSchedules(manha)
@@ -24,6 +47,16 @@ export default function ScheduleTable(props) {
         if (e.target.value === 'noite') {
             setSchedules(noite)
         }
+    }
+    function filtrarMaterial(schedule){
+     let reservaFiltrada = null;
+      reserves.forEach((reserve)=>{
+        if(reserve.data().schedule===schedule&&reserve.data().data===formatarData()){
+            reservaFiltrada = reserve.data().material
+        }
+      })
+      
+      return reservaFiltrada
     }
     return (
         <div className={styles.scheduleTable}>
@@ -47,18 +80,18 @@ export default function ScheduleTable(props) {
                             </h3>
                         </div>
                         <div className={styles.dadosAgendamento}>
-                            <div> Material a ser utilizado : </div>
+                            <div> Material a ser utilizado : {reserves&&`${filtrarMaterial(schedule)}`}</div>
                             <div> Professor: </div>
                             <div className={styles.botaoSelecao}>
-                                <button style={{ backgroundColor: '#338f33' }} onClick={()=>{setModal(!modal)}}> editar </button>
+                                <button style={{ backgroundColor: '#338f33' }} onClick={() => { setModal(!modal) }}> editar </button>
                                 <button style={{ backgroundColor: '#e22424' }}> deletar</button>
                             </div>
                         </div>
 
                     </div>
-                    {modal &&<Modal data={data} schedule={schedule} currentUser={props.currentUser} setModal={setModal} modal={modal}/>}
-                        
-                    
+                    {modal && <Modal data={data} schedule={schedule} currentUser={props.currentUser} setModal={setModal} modal={modal} />}
+
+
                 </>
 
             )) : <h3>Carregando os horários</h3>}
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,43 +1,52 @@
 import { useNavigate } from "react-router-dom";
-
-
-
 import Auth from "../../db/Authentication"
 import { GoogleAuthProvider } from "firebase/auth";
+import styles from './styles.module.scss';
+import {FcGoogle} from 'react-icons/fc'
 const provider = new GoogleAuthProvider();
-export default function Login(){
-    const navigate = useNavigate()
-    const LoginGoogle=()=>{
-         Auth.Login().then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            // IdP data available using getAdditionalUserInfo(result)
-            // ...
-            console.log(credential, '  token:',token,'  _user:', user)
-            navigate('/home')
-          }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-            console.log(errorCode, errorMessage,email, credential)
-          });
-    }
-    
 
-    
-    return(
-        <div>
-            <button 
-            onClick={()=>{LoginGoogle()}}
-            >clique para logar com sua conta Gmail</button>
-        </div>
-    )
+
+export default function Login() {
+  const navigate = useNavigate()
+  const LoginGoogle = () => {
+    Auth.Login().then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+      console.log(credential, '  token:', token, '  _user:', user)
+      navigate('/home')
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+      console.log(errorCode, errorMessage, email, credential)
+    });
+  }
+
+
+
+  return (
+    <div className={styles.backgroundLogin}>
+      <div className={styles.loginContainer}>
+       <h2>
+       Para acessar o agendamento faça o login com o seu Gmail
+        </h2> 
+
+        <button className={styles.botaoLogin}
+          onClick={() => { LoginGoogle() }}
+        ><FcGoogle/> Logar com sua conta Gmail</button>
+
+      </div>
+
+    </div>
+  )
 }
